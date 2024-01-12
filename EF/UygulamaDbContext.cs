9de8a9dbac1ac1ea3ff6bb8a0e0e4d0ce6af3570
Yasin <mustafayasinemir@gmail.com>
2024-01-12 04:10:58 +0300

    Add project files.

---
+++ EF/UygulamaDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Library.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Library.EF
+{
+    public class UygulamaDbContext:DbContext
+
+    {
+        public UygulamaDbContext(DbContextOptions<UygulamaDbContext> options) : base(options)
+        {
+ 
+        }
+        public DbSet<KitapTuru> KitapTurleri { get; set; }
+    }
+}
---
+++ Models/KitapTuru.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Library.Models
+{
+    public class KitapTuru
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required] 
+        public string Ad { get; set; }
+
+
+    }
+}
