09dd32feabdcd1f6a42da4e752c1763beb599cb0
Yasin <mustafayasinemir@gmail.com>
2024-01-12 22:08:57 +0300

    crud operations

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -18,6 +18,7 @@ namespace Library.Controllers
             return View();
         }
 
+
         public IActionResult Privacy()
         {
             return View();
--- Controllers/KitapTuruController.cs
+++ Controllers/KitapTuruController.cs
@@ -1,12 +1,40 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Library.EF;
+using Library.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Library.Controllers
 {
     public class KitapTuruController : Controller
     {
+        //Dependency Injection
+        private readonly UygulamaDbContext _uygulamaDbContext;
+
+
+        public KitapTuruController(UygulamaDbContext context)
+        {
+            _uygulamaDbContext = context;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            List<KitapTuru> objKitapTuruList=_uygulamaDbContext.KitapTurleri.ToList();
+            return View(objKitapTuruList);
         }
-    }
+
+		public IActionResult Ekle()
+		{
+			return View();
+		}
+
+		public IActionResult Guncelle() 
+		{
+			return View();
+		}
+
+
+		public IActionResult Sil()
+		{
+			return View();
+		}
+	}
 }
